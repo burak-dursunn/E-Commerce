3d037a5b548961059d48830af44f7c4470c587e5
Burak Dursun <dursunburak046@gmail.com>
2025-10-29 13:57:15 +0100

    login proccess with jwt tokens

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); //! for password hashing.   
+const bcrypt = require('bcryptjs'); //! for password hashing.
+const jwt = require('jsonwebtoken');
 
 
 
@@ -84,10 +85,38 @@ const delete_user = async (req, res) => {
 
 }
 
+//! Login
+const user_login = async (req,res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email});
+        const secret = process.env.JWT_TOKEN;
+
+        if(!user)
+            return res.status(400).send("User not found")
+        if(user && bcrypt.compare(req.body.password, user.passwordHash)) {
+            const token = jwt.sign({
+                userId: user.id
+            },
+            secret,
+            {expiresIn: '1d'})
+
+            res.status(200).send({user: user.email, token: token})
+        } else {
+            res.status(400).send('the password you entered it is wrong')
+        }
+    
+    } catch (error) {
+        res.status(505).json({success: false, message: error.message});
+    }
+
+}
+
 
 
 module.exports = {
     user_get,
+    get_user_details,
     user_post,
-    delete_user
+    delete_user,
+    user_login,
 }
--- routers/users.js
+++ routers/users.js
@@ -4,10 +4,11 @@ const userController = require('../controllers/userController')
 
 
 router.get('/', userController.user_get)
-
 router.post('/', userController.user_post)
 router.get('/:id', userController.get_user_details)
-
 router.delete('/:id',userController.delete_user);
+
+//! Login
+router.post('/login', userController.user_login);
 
 module.exports = router;
