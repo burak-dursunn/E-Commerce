24dae5d179217b2ad4c9d732c70565ec63f75126
Burak Dursun <dursunburak046@gmail.com>
2025-11-03 00:37:34 +0100

    Refactor order controller to improve order retrieval and detail fetching

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,22 +1,49 @@
 const { Order } = require('../models/order');
 const OrderItem = require('../models/order-item');
 
+const get_orders = async (req, res) => {
+    try {
+        const order = await Order.find().populate({
+            path: 'orderItems',
+            select: '-quantity',
+            populate: {
+                path: 'product',
+                select: 'name price'
+            }
+        });
+        res.status(200).send(order);
 
-const post_order = async (req,res) => {
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+
+    }
+}
+
+
+const get_order_details = async (req, res) => {
+    try {
+        const orderDetails = await Order.findById(req.params.id);
+        res.status(200).send(orderDetails);
+
+    } catch (error) {
+        res.status(404).json({succes: false, message: error.message})
+    }
+}
+const post_order = async (req, res) => {
 
     try {
         //! Save each order item individually.
         const orderItemsIds = await Promise.all(
             req.body.orderItems.map(async orderItem => {
-            let newOrderItem =new OrderItem({
-                quantity: orderItem.quantity,
-                product: orderItem.product
-            })
-            newOrderItem = await newOrderItem.save();
-    
-            return newOrderItem._id;            
-        }))
-    
+                let newOrderItem = new OrderItem({
+                    quantity: orderItem.quantity,
+                    product: orderItem.product
+                })
+                newOrderItem = await newOrderItem.save();
+
+                return newOrderItem._id;
+            }))
+
         //! Create the order.
         const order = new Order({
             orderItems: orderItemsIds,
@@ -30,19 +57,21 @@ const post_order = async (req,res) => {
             totalPrice: req.body.totalPrice,
             user: req.body.user,
         })
-    
+
         const savedOrder = await order.save();
-        if(!savedOrder) {
+        if (!savedOrder) {
             return res.status(500).send('the order could not be saved')
         }
         //* Order Creation Successfull
         res.status(201).send(savedOrder);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ success: false, message: error.message});
+        res.status(500).json({ success: false, message: error.message });
     }
 }
 
 module.exports = {
+    get_orders,
+    get_order_details,
     post_order
 }
--- routers/orders.js
+++ routers/orders.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 
+router.get('/', orderController.get_orders);
+router.get('/:id', orderController.get_order_details);
 router.post('/', orderController.post_order);
 
 
